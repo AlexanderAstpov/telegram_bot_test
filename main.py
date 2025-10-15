6653c81a33ccd384b21b800cf097881277487df8
AlexanderAstapov <1322472@mail.ru>
2025-10-15 17:46:28 +0300

    HT 22.10.25

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram import F
 
+
 import asyncio
 
 
@@ -17,6 +18,20 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def start_handler(message: types.Message):
     await message.answer("Привет! Ты запустил бота")
+
+@dp.message(Command("about"))
+async def about_command(message: types.Message):
+    about_text = "Я бот, созданный для демонстрации обработки команд. Автор: [@AleksanderAstapov]."
+    await message.answer(about_text)
+
+count = 0
+@dp.message(Command("tab"))
+async def tab_command(message: types.Message):
+    global count
+    count = count+1
+    tab_text = f"{count}"
+    await message.answer(f"Вы воспользавались кнопкой Tab - {tab_text} раз")
+
 
 async def main():
     print("Бот запущен...")
